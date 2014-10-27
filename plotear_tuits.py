b5ad3ef8493ddb02738941b0a249de4c061db9a4
aniversarioperu <aniversarioperu1@gmail.com>
2014-10-27 22:36:10 +0200

    nice looking

--- plotear_tuits.py
+++ plotear_tuits.py
@@ -1,21 +1,18 @@
 # -*- coding: utf-8 -*-
-import codecs
-import datetime
-from itertools import groupby
-import json
-import re
-import time
 import sys
 
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
+
 import utils
 
 
-sns.set(style="whitegrid")
-rs = np.random.RandomState(7)
+path = 'RobotoCondensed.ttf'
+prop = fm.FontProperties(fname=path)
+
+sns.set(style="whitegrid", font_scale=.9)
 
 x, y = utils.get_xy_values()
 x = np.array(x)
@@ -23,15 +20,23 @@ y = np.array(y)
 f, ax = plt.subplots()
 
 sns.barplot(x, y, ax=ax)
-ax.set_xlabel('Fecha')
-ax.set_ylabel('Número de tuits por día')
-ax.set_title('Reportes de incendio desde la cuenta @bomberos')
+sns.despine(left=True)
+
+ax.set_xlabel('Fecha',
+              size=14,
+              fontproperties=prop,
+              )
+ax.set_ylabel('Número de tuits por día',
+              size=14,
+              fontproperties=prop,
+              )
+ax.set_title('Reportes de incendio desde la cuenta @bomberos',
+             size=20,
+             fontproperties=prop,
+             )
 
 
 plt.tight_layout()
-plt.savefig('timeline' + sys.argv[1].strip() + '.svg', frameon=None)
-
-
-
+plt.savefig('timeline' + sys.argv[1].strip().replace(".csv", "") + '.svg', frameon=None)
 
 sys.exit()
