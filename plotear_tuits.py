a9d2cbed2dfae677a40b9ff19f77d5a54dc3e2af
aniversarioperu <aniversarioperu1@gmail.com>
2014-12-13 09:18:44 +0200

    plot versus

--- plotear_tuits.py
+++ plotear_tuits.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import datetime
 import sys
 
 import prettyplotlib as ppl
@@ -16,21 +17,38 @@ import utils
 path = 'RobotoCondensed.ttf'
 prop = fm.FontProperties(fname=path)
 
+filenames = [
+    'tuits_incendio_2013.csv',
+    'tuits_incendio_2014.csv',
+]
 
-dates, y = utils.get_xy_values()
-y = np.array(y)
-print(y, len(y))
-x = np.array(range(0, len(dates), 1))
-print(x, len(dates))
+filename = filenames[0]
+
+# dates 2013 will show as 2014 so we can plot the two series together
+dates_2013, y_2013 = utils.get_xy_values(filenames[0])
+dates_2014, y_2014 = utils.get_xy_values(filenames[1])
+
+# need to make sure y_2013 and y_2014 have same number of data points
+# pad with zero
+padding = [0]*(len(y_2014) - len(y_2013))
+y_2013 = padding + y_2013
+
+y_2013 = np.array(y_2013)
+y_2014 = np.array(y_2014)
+
+x_2013 = np.array(range(0, len(dates_2013), 1))
+x_2014 = np.array(range(0, len(dates_2014), 1))
 
 x_labels = []
-for i in dates:
-    x_labels.append(i.date())
+for i in set(dates_2013 + dates_2014):
+    if i.date() not in x_labels:
+        x_labels.append(i.date())
+x_labels.sort()
 
-ind = np.arange(len(y))
+ind = np.arange(len(y_2014))
 fig, ax = plt.subplots(1)
 #rects1 = ax.bar(ind, y, width=0.5, color="#2ecc71", edgecolor="#3498db")
-ax.set_xticks(ind + 0.2)
+ax.set_xticks(ind)
 ax.set_title('Reportes de incendio desde la cuenta @bomberos',
              size=22,
              fontproperties=prop,
@@ -38,13 +56,13 @@ ax.set_title('Reportes de incendio desde la cuenta @bomberos',
 ax.tick_params(axis="x", color="gray", labelsize=8)
 
 ax.set_xticklabels(x_labels)
-plt.ylim([0,max(y) + 2])
+plt.ylim([0, 60])
 plt.xticks(rotation="90")
 
 # queremos color
 set2 = brewer2mpl.get_map('Set2', 'qualitative', 8).mpl_colors
 color = set2[0]
- 
+
 ax.set_xlabel('Fecha',
               size=14,
               fontproperties=prop,
@@ -54,10 +72,9 @@ ax.set_ylabel('Número de tuits por día',
               fontproperties=prop,
               )
 
-plt.plot(x, y, color=color)
+plt.plot(x_2014, y_2013, '#7f8c8d', x_2014, y_2014, '#27ae60')
 plt.tight_layout()
-plt.savefig('timeline' + sys.argv[1].strip().replace(".csv", "") + '.svg', frameon=None)
+plt.savefig('timeline_incendio_2013_vs_2014.svg', frameon=None)
 
-
-
-
+if datetime.datetime(2014, 12, 25) in dates_2014:
+    print("::PLOT IS READY TO PUBLISH::")
--- utils.py
+++ utils.py
@@ -5,8 +5,8 @@ import re
 import datetime
 import time
 
-def get_xy_values():
-    f = codecs.open(sys.argv[1].strip(), "r", "utf-8")
+def get_xy_values(filename):
+    f = codecs.open(filename, "r", "utf-8")
     datos = f.readlines()
     f.close()
 
@@ -20,6 +20,8 @@ def get_xy_values():
             fecha = line[1]
             fecha_split = fecha.split(" ")
             fecha = fecha_split[0]
+            if '2013' in fecha:
+                fecha = fecha.replace('2013', '2014')
             fecha = datetime.datetime.strptime(fecha, '%Y-%m-%d')
             #dia = datetime.datetime.strftime(date_object, '%d %b')
 
@@ -28,3 +30,12 @@ def get_xy_values():
                 x.append(fecha)
     y = [len(list(group)) for key, group in groupby(y)]
     return x[::-1], y[::-1]
+
+
+def join_date_series(dates_2013, dates_2014):
+    dates = dates_2013 + dates_2014
+    dates_set = set(dates)
+    dates_list = list(dates)
+    print("list", len(dates_list), dates_list)
+    return dates_list.sort()
+
